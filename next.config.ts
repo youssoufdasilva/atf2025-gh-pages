0d9cbf8fbea4203693107d28c433882320bbb3ae
Youssouf da Silva <youssoufdasilva@gmail.com>
2025-01-26 10:34:23 +0000

    Update basePath for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/your-repo-name',
+  output: "export",
+  basePath: "/atf2025-gh-pages",
 };
 
 export default nextConfig;
